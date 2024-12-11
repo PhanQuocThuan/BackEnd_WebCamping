603cae9baa80930babafa7ec4470a1c6abfc6075
PhanQuocThuan <phanquocthuan1812@gmail.com>
2024-12-11 21:37:44 +0700

    Update CampingContext.cs
    CampingContext

--- Models/CampingContext.cs
+++ Models/CampingContext.cs
@@ -1,7 +1,23 @@
-﻿namespace CampingEquipment.Models
+using Microsoft.EntityFrameworkCore;
+using WebCamping.Models;
+
+namespace CampingEquipment.Models
 {
-    public class CampingContext
+    public class CampingContext : DbContext
     {
+        public CampingContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AdminUser> AdminUsers { get; set; }
+        public DbSet<Banner> Banners { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Setting> Settings { get; set; }
+        public DbSet<User> Users { get; set; }
 
     }
 }
